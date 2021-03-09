84516fe0a6dd1b1b013f6202594f2f3da36dae05
maguohao2018 <guohao.ma@fit2cloud.com>
2021-03-09 17:15:10 +0800

    feat(install): 修改quick_start.sh

--- riskscanner/quick_start.sh
+++ riskscanner/quick_start.sh
@@ -8,10 +8,11 @@ else
   VERSION=$(curl -s https://github.com/RiskScanner/riskscanner/releases/latest/download 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
 fi
 
-wget --no-check-certificate https://github.com/RiskScanner/riskscanner/releases/latest/download/riskscannerV${VERSION}.tar.gz
+wget --no-check-certificate https://github.com/RiskScanner/riskscanner/releases/latest/download/riskscanner-release-${VERSION}-offline.tar.gz
 #curl -s https://api.github.com/repos/RiskScanner/riskscanner/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
 
-tar zxvf riskscannerV${VERSION}.tar.gz
-cd riskscannerV${VERSION}/installer
+tar zxvf riskscanner-release-${VERSION}-offline.tar.gz
+
+cd riskscanner-release-${VERSION}-offline/installer
 
 /bin/bash install.sh
