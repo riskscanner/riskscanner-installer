352a086481fe6085a36e8f987290a6b1e98cb274
maguohao2018 <guohao.ma@fit2cloud.com>
2021-03-17 19:07:31 +0800

    fix(quick_start): 修改仓库名

--- riskscanner/quick_start.sh
+++ riskscanner/quick_start.sh
@@ -2,12 +2,12 @@
 os=$(uname -a)
 # 支持MacOS
 if [[ $os =~ 'Darwin' ]]; then
-  VERSION=$(curl -s https://github.com/RiskScanner/riskscanner/releases/latest |grep -Eo 'v[0-9]+.[0-9]+.[0-9]+' | tr -d 'a-zA-Z')
+  VERSION=$(curl -s https://github.com/riskscanner/riskscanner/releases/latest |grep -Eo 'v[0-9]+.[0-9]+.[0-9]+' | tr -d 'a-zA-Z')
 else
-  VERSION=$(curl -s https://github.com/RiskScanner/riskscanner/releases/latest/download 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+.*(?=")' | tr -d 'a-zA-Z')
+  VERSION=$(curl -s https://github.com/riskscanner/riskscanner/releases/latest/download 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+.*(?=")' | tr -d 'a-zA-Z')
 fi
 
-wget --no-check-certificate https://github.com/RiskScanner/riskscanner/releases/latest/download/riskscanner-release-${VERSION}.tar.gz
+wget --no-check-certificate https://github.com/riskscanner/riskscanner/releases/latest/download/riskscanner-release-${VERSION}.tar.gz
 
 tar zxvf riskscanner-release-${VERSION}.tar.gz
 
