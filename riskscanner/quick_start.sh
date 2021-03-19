d6075a4082d5ef1a39ed14b698fdf9691758e9e4
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-03-19 16:02:30 +0800

    docs:快速安装脚本

--- riskscanner/quick_start.sh
+++ riskscanner/quick_start.sh
@@ -1,17 +1,24 @@
 #Install Latest Stable RiskScanner Release
 os=$(uname -a)
-# 支持MacOS
+
 if [[ $os =~ 'Darwin' ]]; then
-#  VERSION=$(curl -s https://github.com/riskscanner/riskscanner/releases/latest |grep -Eo 'v[0-9]+.[0-9]+.[0-9]+' | tr -d 'a-zA-Z')
-  echo "暂不支持MacOS"
+  echo 暂时不支持 MacOS 安装
 else
   VERSION=$(curl -s https://github.com/riskscanner/riskscanner/releases/latest/download 2>&1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]+.*(?=")' | tr -d 'a-zA-Z')
 fi
 
-wget --no-check-certificate https://github.com/riskscanner/riskscanner/releases/latest/download/riskscanner-release-${VERSION}.tar.gz
+if [ -z "$VERSION" ]; then
+  echo Please check your network,github is unreachable!
+  exit
+fi
 
-tar zxvf riskscanner-release-${VERSION}.tar.gz
+if [ ! -f ./riskscanner-release-${VERSION}.tar.gz ]; then
+      wget --no-check-certificate https://github.com/riskscanner/riskscanner/releases/latest/download/riskscanner-release-${VERSION}.tar.gz
+fi
 
-cd riskscanner-release-${VERSION}/riskscanner
+if [ ! -f ./riskscanner-release-${VERSION} ]; then
+      tar zxvf riskscanner-release-${VERSION}.tar.gz
+fi
+cd riskscanner-release-${VERSION}
 
-/bin/bash ../install.sh
+/bin/bash ./install.sh
